661bd15a75ec22a0c29a24f718583f42ab9d180a
Gopal1252 <93401686+Gopal1252@users.noreply.github.com>
2023-09-11 02:15:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var tl = gsap.timeline({
+     repeat: -1
+});
+
+tl
+.to(".imagecontainer ",{
+     ease: Expo.easeInOut,
+     width: "100%",
+     duration: 2,
+     stagger: 2
+},'a')
+.to(".imagecontainer ",{
+     ease: Expo.easeInOut,
+     width: "0%",
+     delay: 2.5,
+     duration: 2,
+     stagger: 2
+},'a')
+.to(".text h1",{
+     ease: Expo.easeInOut,
+     duration: 2,
+     stagger: 2,
+     top: 0
+},'a')
+.to(".text h1",{
+     delay: 2,
+     ease: Expo.easeInOut,
+     duration: 2,
+     stagger: 2,
+     top: "-100%"
+},'a')
